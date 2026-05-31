d461a20885ff6bd91e56335ba3fbbccad4142bf4
Suevndu Hatua <suvenduhatua01@gmail.com>
2026-05-31 11:51:29 +0530

    Implemented Eureka server

--- discovery-server/src/main/java/org/programming/microservice/discoveryServer/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/org/programming/microservice/discoveryServer/DiscoveryServerApplication.java
@@ -3,8 +3,10 @@ package org.programming.microservice.discoveryServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryServerApplication {
   public static void main(String[] args) {
     SpringApplication.run(DiscoveryServerApplication.class, args);
