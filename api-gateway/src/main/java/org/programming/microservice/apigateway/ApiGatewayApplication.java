8285972323e93dc068d0fab91ce4c0b2153733e9
Suevndu Hatua <suvenduhatua01@gmail.com>
2026-06-07 13:48:26 +0530

    Deleting yml configuration for defining routes and preferred functional based route configuration

--- api-gateway/src/main/java/org/programming/microservice/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/org/programming/microservice/apigateway/ApiGatewayApplication.java
@@ -2,10 +2,25 @@ package org.programming.microservice.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+      return routeLocatorBuilder.routes()
+          .route("order-service", r -> r.path("api/order/**")
+              .uri("lb://order-service"))
+          .route("product-service", r -> r.path("/api/v1/product/**")
+              .uri("lb://product-service"))
+          .route("inventory-service", r -> r.path("/api/inventory/**")
+              .uri("lb://inventory-service"))
+          .build();
     }
 }
--- configuration-server/src/main/java/org/programming/microservices/configurationserver/ConfigurationServerApplication.java
+++ configuration-server/src/main/java/org/programming/microservices/configurationserver/ConfigurationServerApplication.java
@@ -1,5 +1,6 @@
 package org.programming.microservices.configurationserver;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
@@ -7,6 +8,6 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 public class ConfigurationServerApplication {
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    SpringApplication.run(ConfigurationServerApplication.class, args);
   }
 }
