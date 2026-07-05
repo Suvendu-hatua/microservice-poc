bcbe2d780203fe1bfd7c2b7801ffa879354115fb
Suevndu Hatua <suvenduhatua01@gmail.com>
2026-07-05 11:24:29 +0530

    Added Auth-service in the Api-gateway

--- api-gateway/src/main/java/org/programming/microservice/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/org/programming/microservice/apigateway/ApiGatewayApplication.java
@@ -15,6 +15,8 @@ public class ApiGatewayApplication {
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
       return routeLocatorBuilder.routes()
+          .route("auth-service",r->r.path("/api/v1/auth/**")
+              .uri("lb://auth-service"))
           .route("order-service", r -> r.path("/api/v1/order/**")
               .uri("lb://order-service"))
           .route("product-service", r -> r.path("/api/v1/product/**")
--- api-gateway/src/main/java/org/programming/microservice/apigateway/configuration/SecurityConfiguration.java
+++ api-gateway/src/main/java/org/programming/microservice/apigateway/configuration/SecurityConfiguration.java
@@ -3,7 +3,6 @@ package org.programming.microservice.apigateway.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
@@ -15,10 +14,13 @@ public class SecurityConfiguration {
     http
         .csrf(ServerHttpSecurity.CsrfSpec::disable)
         .authorizeExchange(auth -> auth
+            .pathMatchers(HttpMethod.POST,
+                "/api/v1/auth/login",
+                "/api/v1/auth/register"
+            ).permitAll()
             .pathMatchers(HttpMethod.GET)
             .permitAll()
-            .anyExchange().authenticated())
-        .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+            .anyExchange().authenticated());
     return http.build();
   }
 }
